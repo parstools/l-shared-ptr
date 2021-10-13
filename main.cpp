5f877cd8e68c8fa7957de83fabed9857c1cbf9b6
Andrzej Borucki <contact@earndero.com>
2021-10-13 21:30:39 +0200

    changed type next to Elem1

--- main.cpp
+++ main.cpp
@@ -4,43 +4,40 @@
 using namespace std;
 
 struct Elem1: public Object {
-    Object* next;
+    Elem1* next;
     string cargo;
     Elem1(string cargo) {
         this->cargo = cargo;
     }
     ~Elem1() {
-        cout << "delete Object " << cargo << endl;
-        lshared_release_atomic(&next);
+        cout << "delete Elem1 " << cargo << endl;
+        lshared_release_atomic((Object**)&next);
     }
 };
 
-static Object* make3nonCyclic() {
+static Elem1* make3nonCyclic() {
     Elem1 *elem = new Elem1("A");
-    Object* A;
-    lshared_init_elem(&A, elem);
+    Elem1* A;
+    lshared_init_elem((Object**)&A, elem);
     elem = new Elem1("B");
-    Object* B;
-    lshared_init_elem(&B, elem);
+    Elem1* B;
+    lshared_init_elem((Object**)&B, elem);
     elem = new Elem1("C");
-    Object* C;
-    lshared_init_elem(&C, elem);
-    Object* p = A;
-    lshared_assign(&((Elem1 *) p)->next, &B);
-    p = B;
-    lshared_assign(&((Elem1 *) p)->next, &C);
-    lshared_release_atomic(&B);
-    lshared_release_atomic(&C);
+    Elem1* C;
+    lshared_init_elem((Object**)&C, elem);
+    lshared_assign((Object**)&A->next, (Object**)&B);
+    lshared_assign((Object**)&B->next, (Object**)&C);
+    lshared_release_atomic((Object**)&B);
+    lshared_release_atomic((Object**)&C);
     return A;
 }
 
 static void test2() {
     //non cyclic list of three
-    Object* head = make3nonCyclic();
-    Object*p = head;
-    lshared_assign(&head, &((Elem1 *) p)->next);
+    Elem1* head = make3nonCyclic();
+    lshared_assign((Object**)&head, (Object**)&head->next);
     cout << "elem A should be deleted" << endl;
-    lshared_release_atomic(&head);
+    lshared_release_atomic((Object**)&head);
 }
 
 int main() {
