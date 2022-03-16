b99049720031121bcd8430f71abe4240ee19f27b
Andrzej Borucki <borucki.andrzej@gmail.com>
2022-03-16 16:02:54 +0100

    Mutex and spin and experimental

--- LShared.cpp
+++ LShared.cpp
@@ -1,5 +1,7 @@
 #include <cassert>
+#include <iostream>
 #include "LShared.h"
+
 void lshared_init_elem(Object** pp, Object* p) {
     p->use_count = 1;
     *pp = p;
@@ -9,9 +11,19 @@ void lshared_init_elem(Object* p) {
     p->use_count = 1;
 }
 
-
 void lshared_add_ref_single(Object* p) {
     ++p->use_count;
+}
+
+void lshared_add_ref_mutex(Object* p) {
+    std::lock_guard<std::mutex> guard(p->m_mutex);
+    ++p->use_count;
+}
+
+void lshared_add_ref_spin(Object* p) {
+    p->m_spin.lock();
+    ++p->use_count;
+    p->m_spin.unlock();
 }
 
 void lshared_add_ref_atomic(Object* p) {
@@ -20,11 +32,38 @@ void lshared_add_ref_atomic(Object* p) {
 
 void lshared_release_single(Object** pp) {
     Object *p = *pp;
+    if (!p) return;
     if (--p->use_count == 0)
     {
         *pp = nullptr;
         delete p;
     }
+}
+
+void lshared_release_mutex(Object** pp) {
+    Object *p = *pp;
+    if (!p) return;
+    p->m_mutex.lock();
+    if (--p->use_count == 0)
+    {
+        p->m_mutex.unlock();
+        *pp = nullptr;
+        delete p;
+    }
+    else p->m_mutex.unlock();
+}
+
+void lshared_release_spin(Object** pp) {
+    Object *p = *pp;
+    if (!p) return;
+    p->m_spin.lock();
+    if (--p->use_count == 0)
+    {
+        p->m_spin.unlock();
+        *pp = nullptr;
+        delete p;
+    }
+    else p->m_spin.unlock();
 }
 
 void lshared_release_atomic(Object** pp)  {
@@ -37,6 +76,24 @@ void lshared_release_atomic(Object** pp)  {
         *pp = nullptr;
         delete p;
     }
+}
+
+void lshared_release_break(Object** pp, int maxn)  {
+    Object *p = *pp;
+    if (!p) return;
+    if (p->use_count>maxn)
+        std::cout << "odmawaim bo > czyli "  << p->use_count << std::endl;
+    else {
+        *pp = nullptr;
+        delete p;
+    }
+    // Be race-detector-friendly.  For more info see bits/c++config.
+    /*_GLIBCXX_SYNCHRONIZATION_HAPPENS_BEFORE(&use_count);
+    if (__gnu_cxx::__exchange_and_add_dispatch(&p->use_count, -1) == 1) {
+        _GLIBCXX_SYNCHRONIZATION_HAPPENS_AFTER(&use_count);
+        *pp = nullptr;
+        delete p;
+    }*/
 }
 
 long lshared_count_single(Object* p) {
@@ -71,13 +128,46 @@ void lshared_init_other(Object** pp, Object*__r) {
     *pp = sc;
 }
 
-void    lshared_assign(Object** pp, Object* p) {
+void lshared_assign(Object** pp, Object* p) {
     assert(pp);
     Object*__tmp = p;
     if (__tmp != *pp) {
         if (__tmp != nullptr)
             lshared_add_ref_atomic(__tmp);
         lshared_release_atomic(pp);
+        *pp = __tmp;
+    }
+}
+
+void lshared_assign_mutex(Object** pp, Object* p) {
+    assert(pp);
+    Object*__tmp = p;
+    if (__tmp != *pp) {
+        if (__tmp != nullptr)
+            lshared_add_ref_mutex(__tmp);
+        lshared_release_mutex(pp);
+        *pp = __tmp;
+    }
+}
+
+void lshared_assign_spin(Object** pp, Object* p) {
+    assert(pp);
+    Object*__tmp = p;
+    if (__tmp != *pp) {
+        if (__tmp != nullptr)
+            lshared_add_ref_spin(__tmp);
+        lshared_release_spin(pp);
+        *pp = __tmp;
+    }
+}
+
+void lshared_assign_single(Object** pp, Object* p) {
+    assert(pp);
+    Object*__tmp = p;
+    if (__tmp != *pp) {
+        if (__tmp != nullptr)
+            lshared_add_ref_single(__tmp);
+        lshared_release_single(pp);
         *pp = __tmp;
     }
 }
--- LShared.h
+++ LShared.h
@@ -5,11 +5,45 @@
 #ifndef MYSMARTPTR_LSHARED_H
 #define MYSMARTPTR_LSHARED_H
 
+#include <mutex>
+#include <atomic>
 #include <bits/atomic_word.h>
 #include <ext/atomicity.h>
 
+struct spinlock {
+    std::atomic<bool> lock_ = {0};
+
+    void lock() noexcept {
+        for (;;) {
+            // Optimistically assume the lock is free on the first try
+            if (!lock_.exchange(true, std::memory_order_acquire)) {
+                return;
+            }
+            // Wait for lock to be released without generating cache misses
+            while (lock_.load(std::memory_order_relaxed)) {
+                // Issue X86 PAUSE or ARM YIELD instruction to reduce contention between
+                // hyper-threads
+                __builtin_ia32_pause();
+            }
+        }
+    }
+
+    bool try_lock() noexcept {
+        // First do a relaxed load to check if lock is free in order to prevent
+        // unnecessary cache misses if someone does while(!try_lock())
+        return !lock_.load(std::memory_order_relaxed) &&
+               !lock_.exchange(true, std::memory_order_acquire);
+    }
+
+    void unlock() noexcept {
+        lock_.store(false, std::memory_order_release);
+    }
+};
+
 struct Object {
     _Atomic_word use_count;
+    std::mutex m_mutex;
+    spinlock m_spin;
     virtual ~Object(){}
 };
 
@@ -17,5 +51,15 @@ void lshared_release_atomic(struct Object** pp);
 void lshared_init_elem(Object* p);
 void lshared_assign(struct Object** pp, Object* p);
 void lweak_assign(Object** pp, Object* p);
+void lshared_release_break(Object** pp, int maxn);
+
+void lshared_release_single(Object** pp);
+void lshared_assign_single(Object** pp, Object* p);
+
+void lshared_release_mutex(Object** pp);
+void lshared_assign_mutex(Object** pp, Object* p);
+
+void lshared_release_spin(Object** pp);
+void lshared_assign_spin(Object** pp, Object* p);
 
 #endif //MYSMARTPTR_LSHARED_H
